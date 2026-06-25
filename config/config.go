40277b79bbf2cd50bb0d43f2fbe76a6be345fd1e
marcel <izuobasomtochukwu@gmail.com>
2026-06-25 17:52:53 -0300

    tested cors accept origin issue

--- config/config.go
+++ config/config.go
@@ -57,7 +57,7 @@ func connectRedis (){
 
 func CORSConfig() cors.Config{
   config := cors.DefaultConfig()
-  config.AllowOrigins = []string{"https://prep-mate-xyz.vercel.app/","http://localhost:3000"}
+  config.AllowOrigins = []string{"https://prep-mate-xyz.vercel.app","http://localhost:3000"}
   config.AllowMethods = []string{"GET" , "POST" ,"PUT" , "PATCH" , "DELETE" , "OPTIONS"}
   config.AllowHeaders = []string{"Origin","Content-Type","Authorization"}
   config.AllowCredentials = true
